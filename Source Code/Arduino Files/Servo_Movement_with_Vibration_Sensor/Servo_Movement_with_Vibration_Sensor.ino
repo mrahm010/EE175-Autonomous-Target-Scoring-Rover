1e5da6f3473a78079dae3c7fd94c19a12a77e254
AusRT38 <atopa001@ucr.edu>
2020-02-09 17:03:13 -0800

    Update Servo_Movement_with_Vibration_Sensor.ino

--- Source Code/Arduino Files/Servo_Movement_with_Vibration_Sensor/Servo_Movement_with_Vibration_Sensor.ino
+++ Source Code/Arduino Files/Servo_Movement_with_Vibration_Sensor/Servo_Movement_with_Vibration_Sensor.ino
@@ -1,73 +1,52 @@
 #include <Servo.h>
 
-Servo myservo;  // create servo object to control a servo
-int myservoPIN = 3;
+Servo myservo; 
+int myservoPIN = 3; // Servo on Pin 3
+int vibration_pin = 7; // Vibration Sensor on Pin 7
 
-int angle = 105;    // initial angle  for servo
+int angle = 105;
 int angleStep = 10;
-int vibration_pin = 7;
-
 const int minAngle = 0;
 const int maxAngle = 106;
 
-int targetStart = 1;
+int targetStart = 1; // Lock and Key for Target to Raise when the rover stops
 int targetHIT = 0;
-
-
 int pos = 0;
-
-//int SenseForHit(int &counter) {
-//  int val;
-//  val = digitalRead(vibration_pin);
-//  if(val == 1) {
-//    counter++;
-//  }
-//  else {
-//    if(counter > 0) {
-//      counter--;
-//    }
-//  }
-//  if (counter >= 1000) {
-//    return 1;
-//  }
-//  else {
-//    return 0;
-//  }
-//}
 
 void setup() {
   Serial.begin(9600);      
-  myservo.attach(myservoPIN);  // attaches the servo on pin 3 to the servo object
-  Serial.println("Servo Button ");
+  myservo.attach(myservoPIN);  
+  // Serial.println("Servo Button");
   pinMode(vibration_pin, INPUT);
-  
 }
 
 void TargetSystem() {
- int val;
- val = digitalRead(vibration_pin);
- if (targetStart) {
-  for(pos = 0; pos <= 107; pos += 1) {
-    myservo.write(pos);
-    delay(15);
-  }
+  int val;
+  val = digitalRead(vibration_pin);
+ 
+  if(targetStart) {
+    for(pos = 0; pos <= 107; pos += 1) {
+      myservo.write(pos);
+      delay(15);
+    }
   targetStart = 0;
- }
-    if(val == 1 ){
-        targetHIT = 1; 
-    }    
-    if(targetHIT){ //changes the angle for the next time through the loop
-       angle = angle - angleStep;
-        if (angle <= minAngle || angle >= maxAngle) { // reveres the direction of the moving at the ends of the angle
-         // angleStep = -angleStep;
-          targetHIT = 0;
-        } 
-      myservo.write(angle); // move the servo to desired angle
-      Serial.print("Moved to: ");
-      Serial.print(angle);   // print the angle
-      Serial.println(" degree");    
-      delay(100); // waits for the servo to get there
-   }  
+  }
+  
+  if(val == 1 ) { // Detects the target hit
+     targetHIT = 1; 
+  }    
+  
+  if(targetHIT) { 
+     angle = angle - angleStep;
+     if(angle <= minAngle || angle >= maxAngle) { 
+        targetHIT = 0;
+     } 
+     myservo.write(angle); 
+     Serial.print("Moved to: ");
+     Serial.print(angle);   
+     Serial.println(" degree");    
+     delay(100); 
+  }  
 }
 
 void loop() {
