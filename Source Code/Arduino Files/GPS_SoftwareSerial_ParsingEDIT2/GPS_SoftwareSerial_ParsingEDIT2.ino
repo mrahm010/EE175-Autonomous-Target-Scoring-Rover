954c3c696851a1e15728ca3227f9ed57615a8c31
rlim011 <rlim011@ucr.edu>
2020-02-17 14:14:56 -0800

    GetCoord function

--- Source Code/Arduino Files/GPS_SoftwareSerial_ParsingEDIT2/GPS_SoftwareSerial_ParsingEDIT2.ino
+++ Source Code/Arduino Files/GPS_SoftwareSerial_ParsingEDIT2/GPS_SoftwareSerial_ParsingEDIT2.ino
@@ -1,16 +1,3 @@
-// Test code for Adafruit GPS modules using MTK3329/MTK3339 driver
-//
-// This code shows how to listen to the GPS module in an interrupt
-// which allows the program to have more 'freedom' - just parse
-// when a new NMEA sentence is available! Then access data when
-// desired.
-//
-// Tested and works great with the Adafruit Ultimate GPS module
-// using MTK33x9 chipset
-//    ------> http://www.adafruit.com/products/746
-// Pick one up today at the Adafruit electronics shop
-// and help support open source hardware & software! -ada
-
 #include <Adafruit_GPS.h>
 #include <SoftwareSerial.h>
 
@@ -19,25 +6,17 @@
 // Connect the GPS TX (transmit) pin to Digital 8
 // Connect the GPS RX (receive) pin to Digital 7
 
-// you can change the pin numbers to match your wiring:
-SoftwareSerial mySerial(8, 7);
+SoftwareSerial mySerial(3, 2);                       //change pins if needed
 Adafruit_GPS GPS(&mySerial);
 
-// Set GPSECHO to 'false' to turn off echoing the GPS data to the Serial console
-// Set to 'true' if you want to debug and listen to the raw GPS sentences
-#define GPSECHO  true
+#define GPSECHO  true                               //false to stop echoing of data to console, true for GPS sentences
 
-void setup()
-{
+void setup(){
+  Serial.begin(115200);                            // connect at 115200 so we can read the GPS fast enough and echo without dropping chars
+  delay(2000);
+  Serial.println("GPS Testing");
 
-  // connect at 115200 so we can read the GPS fast enough and echo without dropping chars
-  // also spit it out
-  Serial.begin(115200);
-  delay(5000);
-  Serial.println("Adafruit GPS library basic test!");
-
-  // 9600 NMEA is the default baud rate for Adafruit MTK GPS's- some use 4800
-  GPS.begin(9600);
+  GPS.begin(9600);                                // 9600 NMEA is the default baud rate for Adafruit MTK GPS's- some use 4800
 
   // uncomment this line to turn on RMC (recommended minimum) and GGA (fix data) including altitude
   GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
@@ -46,58 +25,45 @@ void setup()
   // For parsing data, we don't suggest using anything but either RMC only or RMC+GGA since
   // the parser doesn't care about other sentences at this time
 
-  // Set the update rate
-  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);   // 1 Hz update rate
-  // For the parsing code to work nicely and have time to sort thru the data, and
-  // print it out we don't suggest using anything higher than 1 Hz
+  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);    //set update rate: 1 Hz update rate, going over is not suggested for parsing
 
-  // Request updates on antenna status, comment out to keep quiet
-  GPS.sendCommand(PGCMD_ANTENNA);
-
-  delay(1000);
-  // Ask for firmware version
-  mySerial.println(PMTK_Q_RELEASE);
+  //GPS.sendCommand(PGCMD_ANTENNA);            //request updates on antenna if needed
+  //delay(1000);
+  //mySerial.println(PMTK_Q_RELEASE);         //ask for firmware
 }
 
 uint32_t timer = millis();
-void loop()                     // run over and over again
-{
+
+void GetCoordinates() {
   char c = GPS.read();          
-  // if you want to debug, this is a good time to do it!
- if ((c) && (GPSECHO))     //GPSECHO too continue updating coordinates
-   // Serial.write(c);    //this line will output all the NMEA
+ if ((c) && (GPSECHO))                       //GPSECHO too continue updating coordinates
+   // Serial.write(c);                      //output of all the NMEA
 
-  // if a sentence is received, we can check the checksum, parse it...
-  if (GPS.newNMEAreceived()) {
-    // a tricky thing here is if we print the NMEA sentence, or data
-    // we end up not listening and catching other sentences!
-    // so be very wary if using OUTPUT_ALLDATA and trytng to print out data
-    //Serial.println(GPS.lastNMEA());   // this also sets the newNMEAreceived() flag to false
 
-    if (!GPS.parse(GPS.lastNMEA()))   // this also sets the newNMEAreceived() flag to false
-      return;  // we can fail to parse a sentence in which case we should just wait for another
+  if (GPS.newNMEAreceived()) {              //if nmea found,parse
+    if (!GPS.parse(GPS.lastNMEA()))         // this also sets the newNMEAreceived() flag to false
+      return;                               // we can fail to parse a sentence in which case we should just wait for another
   }
 
-  // if millis() or timer wraps around, we'll just reset it
-  if (timer > millis())  timer = millis();
-
-  // approximately every 2 seconds or so, print out the current stats
+  if (timer > millis())  timer = millis();  //reset timer if needed
+  
   if (millis() - timer > 2000) {
-    timer = millis(); // reset the timer
-
-
-   
+    timer = millis();                       // reset the timer
     Serial.print("Fix: "); Serial.print((int)GPS.fix);
     Serial.print(" quality: "); Serial.println((int)GPS.fixquality);
     if (GPS.fix) {
       Serial.print("Location: ");
-      Serial.print(GPS.latitudeDegrees, 4); 
+      Serial.print(GPS.latitudeDegrees, 6); 
       Serial.print(", ");
-      Serial.print(GPS.longitudeDegrees, 4);
+      Serial.print(GPS.longitudeDegrees, 6);
       Serial.print("\n");
 
       Serial.print("Angle: "); Serial.println(GPS.angle);
 
     }
   }
+  
+}
+void loop() {
+  GetCoordinates();
 }
