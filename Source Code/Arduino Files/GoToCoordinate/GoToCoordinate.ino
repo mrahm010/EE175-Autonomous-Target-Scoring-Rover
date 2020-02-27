c96afb69902d817f3db18096147b9184c6eed49f
mrahm010 <mrahm010@ucr.edu>
2020-02-26 22:33:32 -0800

    Update GoToCoordinate.ino

--- Source Code/Arduino Files/GoToCoordinate/GoToCoordinate.ino
+++ Source Code/Arduino Files/GoToCoordinate/GoToCoordinate.ino
@@ -8,7 +8,7 @@
 #include <Adafruit_GPS.h>
 #include <SoftwareSerial.h>
 #include <Servo.h>
-Adafruit_GPS GPS(&Serial3);
+Adafruit_GPS GPS(&Serial2);
 uint32_t timer = millis();
 
 #define GPSECHO  true
@@ -17,13 +17,23 @@ Adafruit_BNO055 bno = Adafruit_BNO055(55);
 adafruit_bno055_offsets_t calib;
 sensors_event_t event;
 //Chung
-float lat1prec = 0;
-float long1prec = 0;
-float lat1 = 0;
-float long1 = 0;
+float originLat = 0;
+float originLong = 0;
+int originKey = 1;
+
+float currlat1prec = 0;
+float currlong1prec = 0;
+float currlat1 = 0;
+float currlong1 = 0;
 // HUB
-float lat2 = 33.9757;
+float lat2 = 33.9758;
 float long2 = -117.3294;
+
+float lat3 = 33.9757;
+float long3 = -117.3294;
+
+float lat4 = 33.9757;
+float long4 = -117.3296;
 
 /////////////////////////////////////////
 //Global Variables
@@ -41,7 +51,7 @@ int myservoPIN  = 5;
 unsigned long duration;
 int angle = 105;    // initial angle  for servo
 int angleStep = 10;
-int vibration_pin = 39;
+int vibration_pin = 8;
 const int minAngle = 0;
 const int maxAngle = 106;
 int targetStart = 1;
@@ -53,13 +63,13 @@ Servo myservo;
 /* 
 L298N H-Bridge driving DC motor on Arduino
 */
-int ENA = 10; // MCU PWM Pin 10 to ENA on L298n Board
-int IN1 = 13;  // MCU Digital Pin 9 to IN1 on L298n Board 
-int IN2 = 12;  // MCU Digital Pin 8 to IN2 on L298n Board
+int ENA = 13; // MCU PWM Pin 10 to ENA on L298n Board
+int IN1 = 12;  // MCU Digital Pin 9 to IN1 on L298n Board 
+int IN2 = 11;  // MCU Digital Pin 8 to IN2 on L298n Board
 
-int ENB = 6;  // MCU PWM Pin 5 to ENB on L298n Board
-int IN3 = 11;  // MCU Digital pin 7 to IN3 on L298n Board
-int IN4 = 10;  // MCU Digital pin 6 to IN4 on L298n Board
+int ENB = 4;  // MCU PWM Pin 5 to ENB on L298n Board
+int IN3 = 10;  // MCU Digital pin 7 to IN3 on L298n Board
+int IN4 = 9;  // MCU Digital pin 6 to IN4 on L298n Board
 
 void setup()
 {
@@ -138,17 +148,20 @@ int GetCoordinates() {
       Serial.print(", ");
       Serial.print(GPS.longitudeDegrees, 6);
       Serial.print("\n");
-//      lat1 = (round(GPS.latitudeDegrees * 10000)) /10000;
-//      long1 = (round(GPS.longitudeDegrees * 10000)) / 10000;
       counter++;
-//      if (Serial.available() > 0) {
-//        confirm = Serial.read();
-//      }
       if (counter > 1) {
-        lat1prec = GPS.latitudeDegrees;
-        long1prec = GPS.longitudeDegrees;
-        lat1 = floor(GPS.latitudeDegrees*10000 + 0.5)/10000;
-        long1 = floor(GPS.longitudeDegrees*10000 + 0.5)/10000;
+        if (originKey == 1) {
+          originLat = floor(GPS.latitudeDegrees*10000 + 0.5)/10000;
+          originLong = floor(GPS.longitudeDegrees*10000 + 0.5)/10000;
+          originKey = 0;
+          Serial.print("Origin: ");
+          Serial.print(originLat, 4);
+          Serial.print(originLong, 4);
+        }
+        currlat1prec = GPS.latitudeDegrees;
+        currlong1prec = GPS.longitudeDegrees;
+        currlat1 = floor(GPS.latitudeDegrees*10000 + 0.5)/10000;
+        currlong1 = floor(GPS.longitudeDegrees*10000 + 0.5)/10000;
         counter = 0;
         return 1;
       }
@@ -160,6 +173,7 @@ int GetCoordinates() {
   return 0;
 }
 void GoToCoordinate(float latitude, float longitude) {
+  char c;
   float functionAngle = 0;
   float currentLatprec = 0;
   float currentLongprec = 0;
@@ -174,21 +188,26 @@ void GoToCoordinate(float latitude, float longitude) {
   int adjustmentCtr = 0;
   imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
   while (coordinateReached == 0) {
+    //c = GPS.read();
     while(!GetCoordinates()) {
-      
     }
     Serial.print("Precise Lat: ");
-    Serial.println(lat1prec, 6);
+    Serial.println(currlat1prec, 6);
     Serial.print("Precise Long: ");
-    Serial.println(long1prec, 6);
+    Serial.println(currlong1prec, 6);
     Serial.print("currentLat: ");
-    Serial.println(lat1, 4);
+    Serial.println(currlat1, 4);
     Serial.print("currentLong: ");
-    Serial.println(long1, 4);
+    Serial.println(currlong1, 4);
     Serial.print("angle:");
-    functionAngle = anglecalc(lat1prec, latitude, long1prec, longitude);
+    functionAngle = anglecalc(currlat1prec, latitude, currlong1prec, longitude);
     Serial.println(functionAngle);
-    if((lat1 == latitude) && (long1 == longitude)) {
+//    if((abs(currlat1prec) <= abs(latitude) + 0.00002) && 
+//       (abs(currlat1prec) >= abs(latitude) - 0.00002) &&
+//       (abs(currlong1prec) <= abs(longitude) + 0.00002) &&
+//       (abs(currlong1prec) >= abs(longitude) + 0.00002)){
+    if((currlat1 == latitude) && 
+       (currlong1 == longitude)) {
       coordinateReached = 1;
       return;
     }
@@ -203,6 +222,9 @@ void GoToCoordinate(float latitude, float longitude) {
 //        else if ((euler.x() < 30) && (functionAngle > 330)) {
 //          adjustment2();
 //        }
+        if(functionAngle == 0) {
+          leftTurn(10);
+        }
         if (euler.x() > functionAngle) {
           leftTurn(functionAngle);
         }
@@ -283,8 +305,8 @@ void go()
 }
 void forward(int time)
 {
-  analogWrite(ENA, 200);
-  analogWrite(ENB, 200);
+  analogWrite(ENA, 255);
+  analogWrite(ENB, 255);
   digitalWrite(IN1, HIGH);
   digitalWrite(IN2, LOW);  
   digitalWrite(IN3, HIGH);
@@ -294,8 +316,8 @@ void forward(int time)
 
 void backward(int time)
 {
-  analogWrite(ENA, 200);
-  analogWrite(ENB, 200);
+  analogWrite(ENA, 255);
+  analogWrite(ENB, 255);
   digitalWrite(IN1, LOW);
   digitalWrite(IN2, HIGH);  
   digitalWrite(IN3, LOW);
@@ -322,8 +344,8 @@ void adjustment2() {
 
 void right(int time)
 {
-  //analogWrite(ENA, speed);
-  //analogWrite(ENB, speed);
+//  analogWrite(ENA, 255);
+//  analogWrite(ENB, 255);
   digitalWrite(IN1, HIGH);
   digitalWrite(IN2, LOW);  
   digitalWrite(IN3, LOW);
@@ -351,21 +373,37 @@ void brake(int time)
 }
 void rightTurn(float turnangle)
 {
+  float currAngle = 0;
+  float firstAngle = 0;
+  int axisPass = 0;
+  int axisCtr = 0;
   Serial.println("Right turn function");
   imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
-  while(euler.x() < turnangle){
+  delay(100);
+  firstAngle = euler.x();
+  Serial.print("First Angle: ");
+  Serial.print(firstAngle);
+  while((euler.x() < turnangle) && (!axisPass)){
     euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
+    delay(10);
     Serial.print("eulerx: ");
     Serial.println(euler.x());
-    Serial.print("angle: ");
-    Serial.println(turnangle);
-    right(100);
+    currAngle = euler.x();
+    axisCtr++;
+    if((currAngle < firstAngle) && (axisCtr > 3)){
+      axisPass = 1;
+      axisCtr = 0;
+    }
+    right(75);
     brake(100);
   }
   euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
   if (euler.x() >= turnangle)
   {
+    delay(10);
     brake(100);
+    Serial.print("End Angle: ");
+    Serial.println(euler.x());
   }
 //  else if (event.orientation.x - startOrientation > angle)
 //  {
@@ -379,16 +417,18 @@ void leftTurn(float turnangle) {
   float xeuler = euler.x();
   while(xeuler > turnangle){
     euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
+    delay(10);
     Serial.print("eulerx: ");
     Serial.println(xeuler);
     Serial.print("angle: ");
     Serial.println(turnangle);
     xeuler = euler.x();
-    left(100);
-    brake(100);
+    left(75);
+    brake(200);
   }
   euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
   xeuler = euler.x();
+  delay(10);
   if (xeuler <= turnangle)
   {
     brake(100);
@@ -418,8 +458,37 @@ int calibrate() {
   return mag;
 }
 
+void speedControl() {
+  // Turn on motors
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+  
+  // Accelerate from zero to maximum speed
+  for (int i = 0; i < 256; i++) {
+    analogWrite(ENA, i);
+    analogWrite(ENB, i);
+    delay(20);
+  }
+  delay(2000);
+  // Decelerate from maximum speed to zero
+  for (int i = 255; i >= 0; --i) {
+    analogWrite(ENA, i);
+    analogWrite(ENB, i);
+    delay(20);
+  }
+  
+  // Now turn off motors
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+}
+
 void loop()
 {
+    testMotor();
     float turningangle = 0.0;
     imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
     while(!calibrate()) {
@@ -428,10 +497,8 @@ void loop()
     while(!GetCoordinates()) {
     }
     Serial.println("Fix!");
-    Serial.println("Quality at least 2");
     delay(1000);
-    Serial.print("starting...");
-    delay(3000);
+    Serial.println("starting");
     while (key == 1) {
       euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
       startOrientation = euler.x();
@@ -441,14 +508,14 @@ void loop()
     }
     key = 1;
     Serial.print("lat1: ");
-    Serial.println(lat1, 4);
+    Serial.println(currlat1, 4);
     Serial.print("long1: ");
-    Serial.println(long1, 4);
+    Serial.println(currlong1, 4);
     
-    turningangle = anglecalc(lat1prec, lat2, long1prec, long2);
-    Serial.print("startOrientation: ");
+    turningangle = anglecalc(currlat1prec, lat2, currlong1prec, long2);
+    Serial.print("startOrientation (unplug): ");
     Serial.println(startOrientation);
-    delay(1000);
+    delay(6000);
     if (startOrientation > turningangle) {
         leftTurn(turningangle);
     }
@@ -456,22 +523,62 @@ void loop()
         rightTurn(turningangle);
     }
     GoToCoordinate(lat2, long2); 
+    Serial.println("Coordinate Reached!");
     brake(1000);
-//    turningangle = anglecalc(lat2, lat1, long2, long1);
-//    while (key == 1) {
-//      euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
-//      startOrientation = euler.x();
-//      if(startOrientation != 0.0) {
-//        key = 0;
-//      }
-//    }
-//    key = 1;
-//    if (startOrientation > turningangle) {
-//        leftTurn(turningangle);
-//    }
-//    else if (startOrientation < turningangle) {
-//        rightTurn(turningangle);
-//    }
+    turningangle = anglecalc(currlat1, originLat, currlong1, originLong);
+    while (key == 1) {
+      euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
+      startOrientation = euler.x();
+      if(startOrientation != 0.0) {
+        key = 0;
+      }
+    }
+    key = 1;
+    if (startOrientation > turningangle) {
+        leftTurn(turningangle);
+    }
+    else if (startOrientation < turningangle) {
+        rightTurn(turningangle);
+    }
     TargetSystem();
-  
+    GoToCoordinate(originLat, originLong);
+//----------------2 Points---------------------------
+    GoToCoordinate(lat3, long3);
+    brake(1000);
+    turningangle = anglecalc(currlat1, originLat, currlong1, originLong);
+    while (key == 1) {
+      euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
+      startOrientation = euler.x();
+      if(startOrientation != 0.0) {
+        key = 0;
+      }
+    }
+    key = 1;
+    if (startOrientation > turningangle) {
+        leftTurn(turningangle);
+    }
+    else if (startOrientation < turningangle) {
+        rightTurn(turningangle);
+    }
+    TargetSystem();
+    GoToCoordinate(lat4, long4);
+    brake(1000);
+    turningangle = anglecalc(currlat1, originLat, currlong1, originLong);
+    while (key == 1) {
+      euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
+      startOrientation = euler.x();
+      if(startOrientation != 0.0) {
+        key = 0;
+      }
+    }
+    key = 1;
+    if (startOrientation > turningangle) {
+        leftTurn(turningangle);
+    }
+    else if (startOrientation < turningangle) {
+        rightTurn(turningangle);
+    }
+    TargetSystem();
+    GoToCoordinate(originLat, originLong);
+    brake(1000);
 }
